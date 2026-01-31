2ff6991c560a06807759d31caf1d2401eb7ea8aa
hariniHimanshi <gunarathnehmhh.21@itfac.mrt.ac.lk>
2026-02-01 00:58:59 +0530

    Fix quality issues

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -6,11 +6,11 @@ public static void main(String[] args) throws Exception {
 
         Calculator calc = new Calculator();
 
-        System.out.println(calc.calculate(10, 5, "add-again"));
+        System.out.println(calc.calculate(10, 5, "add"));
 
         UserService service = new UserService();
         service.findUser("admin");
-        service.deleteUser("admin"); // NEW dangerous call
+
     }
 }
 
